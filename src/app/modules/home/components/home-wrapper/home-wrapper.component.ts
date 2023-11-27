25700d5f709c6162f75699f6a5adef62121634a6
Saksham Koul <sxhmkoul@gmail.com>
2023-11-28 00:41:11 +0500

    importing fonts

--- src/app/modules/home/components/home-wrapper/home-wrapper.component.ts
+++ src/app/modules/home/components/home-wrapper/home-wrapper.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LinuxNavbarComponent } from '../linux-navbar/linux-navbar.component';
 import { HomeLandingComponent } from '../home-landing/home-landing.component';
 import { SectionComponent } from '../section/section.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-home-wrapper',
   standalone: true,
-  imports: [CommonModule, LinuxNavbarComponent, SectionComponent],
+  imports: [CommonModule, LinuxNavbarComponent, SectionComponent, ButtonComponent],
   templateUrl: './home-wrapper.component.html',
   styleUrl: './home-wrapper.component.scss',
 })
---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() type?: string = 'default';
+
+}
